5d8cf7c9103d44e03c99b5a6c6e1f1ce122b745f
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2016-05-01 15:54:01 +1000

    added starter code for the capstone project

---
+++ app/src/test/java/capstone/android/udacity/edu/udacity_android_project_capstone/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package capstone.android.udacity.edu.udacity_android_project_capstone;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}

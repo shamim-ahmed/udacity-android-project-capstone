a2bf3679753b63d9d07277d321d5d88076f45b21
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2016-05-01 16:43:55 +1000

    added mechanism to read configuration values from a properties file

--- app/src/main/java/edu/udacity/android/capstone/CapstoneApplication.java
+++ app/src/main/java/edu/udacity/android/capstone/CapstoneApplication.java
@@ -1,9 +1,33 @@
 package edu.udacity.android.capstone;
 
 import android.app.Application;
+import android.content.Context;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.Properties;
 
 /**
  * Created by shamim on 5/1/16.
  */
 public class CapstoneApplication extends Application {
+    private static final String TAG = CapstoneApplication.class.getSimpleName();
+    private final Properties configProperties = new Properties();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Context context = getApplicationContext();
+
+        try {
+            configProperties.load(context.getResources().openRawResource(R.raw.config));
+        } catch (IOException ex) {
+            Log.e(TAG, "error while loading configuration properties");
+        }
+    }
+
+    public String getProperty(String key) {
+        return configProperties.getProperty(key);
+    }
 }

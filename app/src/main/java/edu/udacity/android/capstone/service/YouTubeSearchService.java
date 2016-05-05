7c08edfd9b4548132c4d0cd5f3dc1d21cd2aab05
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2016-05-06 08:15:52 +1000

    implemented basic youtube search functionality

--- app/src/main/java/edu/udacity/android/capstone/service/YouTubeSearchService.java
+++ app/src/main/java/edu/udacity/android/capstone/service/YouTubeSearchService.java
@@ -1,13 +1,35 @@
 package edu.udacity.android.capstone.service;
 
 import android.app.Activity;
+import android.net.Uri;
+
+import edu.udacity.android.capstone.CapstoneApplication;
+import edu.udacity.android.capstone.task.SearchTask;
+import edu.udacity.android.capstone.task.YoutubeSearchTask;
 
 /**
  * Created by shamim on 5/1/16.
  */
+//https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyCeBaDHs2Ft4mwuAA1zXYlIwRwjE6yHbjw&q=tulip
 public class YouTubeSearchService implements SearchService {
     @Override
     public void performSearch(String keyword, Activity activity) {
+        CapstoneApplication application = (CapstoneApplication) activity.getApplication();
+        String scheme = application.getProperty("search.api.scheme.youtube");
+        String authority = application.getProperty("search.api.scheme.host");
+        String path = application.getProperty("search.api.path.youtube");
+        String searchKey = application.getProperty("search.api.key.youtube");
 
+        Uri searchUri = new Uri.Builder()
+                .scheme(scheme)
+                .authority(authority)
+                .path(path)
+                .appendQueryParameter("key", searchKey)
+                .appendQueryParameter("part", "snippet")
+                .appendQueryParameter("q", keyword)
+                .build();
+
+        SearchTask searchTask = new YoutubeSearchTask(activity);
+        searchTask.execute(searchUri.toString());
     }
 }
---
+++ app/src/main/java/edu/udacity/android/capstone/task/YoutubeSearchTask.java
@@ -0,0 +1,18 @@
+package edu.udacity.android.capstone.task;
+
+import android.app.Activity;
+
+/**
+ * Created by shamim on 5/6/16.
+ */
+public class YoutubeSearchTask extends SearchTask {
+    public YoutubeSearchTask(Activity activity) {
+        super(activity);
+    }
+
+    @Override
+    public void onPostExecute(String result) {
+        super.onPostExecute(result);
+        // TODO update some view associated with the activity
+    }
+}

9c265d87a63ce6fe5447453d5094facd7407872e
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2016-05-09 21:33:15 +1000

    implemented necessary operations in the database utility class

--- app/src/main/java/edu/udacity/android/contentfinder/db/ContentFinderContract.java
+++ app/src/main/java/edu/udacity/android/contentfinder/db/ContentFinderContract.java
@@ -3,13 +3,18 @@ package edu.udacity.android.contentfinder.db;
 import android.content.ContentResolver;
 import android.net.Uri;
 import android.provider.BaseColumns;
+import android.util.Log;
 
 import java.util.List;
+
+import edu.udacity.android.contentfinder.model.ContentType;
 
 /**
  * Created by shamim on 5/6/16.
  */
 public class ContentFinderContract {
+    private static final String TAG = ContentFinderContract.class.getSimpleName();
+
     public static final String CONTENT_AUTHORITY = "edu.udacity.android.contentfinder";
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
 
@@ -22,7 +27,6 @@ public class ContentFinderContract {
         public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + PATH_TAG;
         public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + PATH_TAG;
 
-
         public static String getTagNameFromUri(Uri uri) {
             List<String> segmentList = uri.getPathSegments();
 
@@ -32,11 +36,16 @@ public class ContentFinderContract {
 
             return segmentList.get(1);
         }
+
+        public static Uri buildUriFromTagName(String tagName) {
+            return BASE_CONTENT_URI.buildUpon().appendPath(PATH_TAG).appendPath(tagName).build();
+        }
     }
 
     public static class ContentEntry implements BaseColumns {
         public static final String TABLE_NAME = "Content";
         public static final String COLUMN_CONTENT_ID = "content_id";
+        public static final String COLUMN_CONTENT_TYPE = "type";
         public static final String COLUMN_TITLE = "title";
         public static final String COLUMN_SUMMARY = "summary";
         public static final String COLUMN_URL = "url";
@@ -45,10 +54,72 @@ public class ContentFinderContract {
 
         public static final String PATH_CONTENT = "content";
         public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendEncodedPath(PATH_CONTENT).build();
-                '
+        public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + PATH_CONTENT;
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + PATH_CONTENT;
 
+        /**
+         * @param uri the input URI of the form /content/123
+         * @return the id of the content
+         */
+        public static Long getContentIdFromUri(Uri uri) {
+            List<String> segmentList = uri.getPathSegments();
 
+            if (segmentList.size() < 2) {
+                return null;
+            }
+
+            Long contentId = null;
+
+            try {
+                contentId = Long.valueOf(segmentList.get(1));
+            } catch (Exception ex) {
+                Log.e(TAG, "error while parsing content id");
+            }
+
+            return contentId;
+        }
+
+        /**
+         * @param uri the input URI of the form /content/photo or /content/photo/tag/cat
+         * @return the content type
+         */
+        public static ContentType getContentTypeFromUri(Uri uri) {
+            List<String> segmentList = uri.getPathSegments();
+
+            if (segmentList.size() < 2) {
+                return null;
+            }
+
+            return ContentType.valueOf(segmentList.get(1));
+        }
+
+        /**
+         * @param uri the input URI of the form /content/photo/tag/cat
+         * @return the tag name
+         */
+        public static String getTagNamFromUri(Uri uri) {
+            List<String> segmentList = uri.getPathSegments();
+
+            if (segmentList.size() < 4) {
+                return null;
+            }
+
+            return segmentList.get(3);
+        }
+
+        public static Uri buildUriFromContentId(Long contentId) {
+            return BASE_CONTENT_URI.buildUpon()
+                    .appendPath(PATH_CONTENT)
+                    .appendPath(contentId.toString())
+                    .build();
+        }
+
+        public static Uri buildUriFromContentTypeAndTagName(ContentType contentType, String tagName) {
+            return BASE_CONTENT_URI.buildUpon()
+                    .appendPath(contentType.toString())
+                    .appendPath(TagEntry.PATH_TAG)
+                    .appendPath(tagName)
+                    .build();
+        }
     }
-
-
 }
--- app/src/main/java/edu/udacity/android/contentfinder/model/ContentType.java
+++ app/src/main/java/edu/udacity/android/contentfinder/model/ContentType.java
@@ -4,5 +4,16 @@ package edu.udacity.android.contentfinder.model;
  * Created by shamim on 5/4/16.
  */
 public enum ContentType {
-    NEWS, PHOTO, TWEET, VIDEO
+    NEWS("news"), PHOTO("photo"), TWEET("tweet"), VIDEO("video");
+
+    private String name;
+
+    private ContentType(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }

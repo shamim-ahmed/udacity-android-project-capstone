16d0c0251d08d3f20efc8f109b030f5b0c43666f
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2016-06-18 14:48:47 +1000

    removed the FAB temporarily

--- app/src/main/java/edu/udacity/android/contentfinder/MainActivityFragment.java
+++ app/src/main/java/edu/udacity/android/contentfinder/MainActivityFragment.java
@@ -1,12 +1,10 @@
 package edu.udacity.android.contentfinder;
 
-import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
 import edu.udacity.android.contentfinder.service.BingSearchService;
 
@@ -15,22 +13,11 @@ import edu.udacity.android.contentfinder.service.BingSearchService;
  */
 public class MainActivityFragment extends Fragment {
 
-    public MainActivityFragment() {
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-        ListView resultListView =  (ListView) rootView.findViewById(R.id.result_list);
-        FloatingActionButton fab = (FloatingActionButton) rootView.findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                BingSearchService.getInstance().performSearch("us election 2016", getActivity());
-            }
-        });
+        BingSearchService.getInstance().performSearch("us election 2016", getActivity());
 
         return rootView;
     }

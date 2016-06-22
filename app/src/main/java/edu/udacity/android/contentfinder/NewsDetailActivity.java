9a697ebc114cd3bcbee6345dc4d01821eb305037
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2016-06-22 21:58:04 +1000

    rearranged UI elements in article detail view

--- app/src/main/java/edu/udacity/android/contentfinder/NewsDetailActivity.java
+++ app/src/main/java/edu/udacity/android/contentfinder/NewsDetailActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -23,7 +24,7 @@ public class NewsDetailActivity extends AppCompatActivity {
         Bundle bundle = getIntent().getExtras();
 
         if (bundle != null) {
-            SearchResult resultItem =  (SearchResult) bundle.get(Constants.SELECTED_NEWS_KEY);
+            SearchResult resultItem = (SearchResult) bundle.get(Constants.SELECTED_NEWS_KEY);
 
             if (resultItem != null) {
                 TextView titleView = (TextView) findViewById(R.id.news_detail_title);
@@ -33,7 +34,10 @@ public class NewsDetailActivity extends AppCompatActivity {
                 descriptionView.setText(resultItem.getDescription());
 
                 TextView sourceView = (TextView) findViewById(R.id.news_detail_source);
-                sourceView.setText(resultItem.getSource());
+                sourceView.setText(String.format("Source : %s", resultItem.getSource()));
+
+                TextView webUrlView = (TextView) findViewById(R.id.news_detail_web_url);
+                webUrlView.setText(Html.fromHtml(String.format("Read the full article <a href=\"%s\">here</a>", resultItem.getWebUrl())));
             }
         }
 

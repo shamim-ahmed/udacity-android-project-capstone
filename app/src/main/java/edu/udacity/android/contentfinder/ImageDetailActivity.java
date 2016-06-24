23cf1770a9b776bbfb2d4cc668500fa6913b34c3
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2016-06-24 21:55:44 +1000

    now the image detail activity displays relevant fields

--- app/src/main/java/edu/udacity/android/contentfinder/ImageDetailActivity.java
+++ app/src/main/java/edu/udacity/android/contentfinder/ImageDetailActivity.java
@@ -6,6 +6,13 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import edu.udacity.android.contentfinder.util.Constants;
+import edu.udacity.android.contentfinder.util.SearchResult;
 
 public class ImageDetailActivity extends AppCompatActivity {
 
@@ -15,6 +22,27 @@ public class ImageDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_image_detail);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        TextView titleView = (TextView) findViewById(R.id.image_detail_title);
+        TextView sourceView = (TextView) findViewById(R.id.image_detail_source);
+        ImageView imageView = (ImageView) findViewById(R.id.image_detail_binary);
+
+        Bundle bundle = getIntent().getExtras();
+
+        if (bundle != null) {
+            SearchResult resultItem = (SearchResult) bundle.get(Constants.SELECTED_IMAGE_KEY);
+
+            if (resultItem != null) {
+                Picasso.with(this)
+                        .load(resultItem.getWebUrl())
+                        .noFade()
+                        .into(imageView);
+
+                titleView.setText(resultItem.getTitle());
+                sourceView.setText(resultItem.getSource());
+            }
+
+        }
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {

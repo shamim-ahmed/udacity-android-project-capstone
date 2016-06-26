765d98dc682e16ec66c6b7623e22a67f317d7829
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2016-06-26 10:42:01 +1000

    added a new field in Keyword

--- app/src/main/java/edu/udacity/android/contentfinder/model/Keyword.java
+++ app/src/main/java/edu/udacity/android/contentfinder/model/Keyword.java
@@ -1,11 +1,14 @@
 package edu.udacity.android.contentfinder.model;
 
+import java.util.Date;
+
 /**
  * Created by shamim on 5/31/16.
  */
 public class Keyword {
     private Long id;
     private String word;
+    private Date createdDate;
 
     public Long getId() {
         return id;
@@ -21,5 +24,13 @@ public class Keyword {
 
     public void setWord(String word) {
         this.word = word;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
     }
 }

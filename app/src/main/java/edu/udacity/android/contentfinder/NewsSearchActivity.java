0dde390fe32891baeffe02a63f14fbaa6095c3aa
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2016-06-28 08:31:08 +1000

    removed fab from news and image search screens

--- app/src/main/java/edu/udacity/android/contentfinder/NewsSearchActivity.java
+++ app/src/main/java/edu/udacity/android/contentfinder/NewsSearchActivity.java
@@ -28,15 +28,6 @@ public class NewsSearchActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         ListView newsList = (ListView) findViewById(R.id.news_list);
 
         newsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {

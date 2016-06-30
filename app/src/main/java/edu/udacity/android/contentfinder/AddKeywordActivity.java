e91af5d5f00f271faaea3b56eb0e277b8703ff88
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2016-06-30 20:39:52 +1000

    removed the share button from add keyword screen

--- app/src/main/java/edu/udacity/android/contentfinder/AddKeywordActivity.java
+++ app/src/main/java/edu/udacity/android/contentfinder/AddKeywordActivity.java
@@ -1,12 +1,8 @@
 package edu.udacity.android.contentfinder;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.EditText;
 
 public class AddKeywordActivity extends AppCompatActivity {
 
@@ -16,16 +12,5 @@ public class AddKeywordActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_keyword);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
     }
-
 }

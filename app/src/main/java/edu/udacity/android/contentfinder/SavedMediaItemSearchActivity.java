fab16bbc823f80816f625f94f68c344c5f1d2876
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2016-08-12 20:54:37 +1000

    Now clicking on saved media items results in the launch of the right detail activity

--- app/src/main/java/edu/udacity/android/contentfinder/SavedMediaItemSearchActivity.java
+++ app/src/main/java/edu/udacity/android/contentfinder/SavedMediaItemSearchActivity.java
@@ -1,5 +1,7 @@
 package edu.udacity.android.contentfinder;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -15,11 +17,14 @@ import edu.udacity.android.contentfinder.model.Keyword;
 import edu.udacity.android.contentfinder.model.MediaItem;
 import edu.udacity.android.contentfinder.task.db.SearchMediaItemTask;
 import edu.udacity.android.contentfinder.ui.MediaItemListAdapter;
+import edu.udacity.android.contentfinder.util.Constants;
 
 /**
  * Created by shamim on 8/7/16.
  */
 public class SavedMediaItemSearchActivity extends AbstractMediaItemSearchActivity implements MediaItemListContainer {
+    private static final String TAG = SavedMediaItemSearchActivity.class.getSimpleName();
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,17 +35,42 @@ public class SavedMediaItemSearchActivity extends AbstractMediaItemSearchActivit
         // display the back button
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        final Spinner keywordSpinner = getKeywordSpinner();
         final ListView savedMediaItemListView = getMediaItemListView();
+
         savedMediaItemListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                savedMediaItemListView.getItemAtPosition(position);
+                MediaItem mediaItem = (MediaItem) savedMediaItemListView.getItemAtPosition(position);
+                Keyword keyword = (Keyword) keywordSpinner.getSelectedItem();
+                Class<? extends Activity> activityClass = null;
+
+                switch (mediaItem.getContentType()) {
+                    case NEWS: {
+                        activityClass = NewsDetailActivity.class;
+                        break;
+                    }
+                    case PHOTO: {
+                        activityClass = ImageDetailActivity.class;
+                        break;
+                    }
+                    case VIDEO: {
+                        activityClass = YouTubeVideoDetailActivity.class;
+                        break;
+                    }
+                    default: {
+                        break;
+                    }
+                }
+
+                Intent intent = new Intent(SavedMediaItemSearchActivity.this, activityClass);
+                intent.putExtra(Constants.SELECTED_MEDIA_ITEM, mediaItem);
+                intent.putExtra(Constants.SELECTED_KEYWORD, keyword);
+                startActivity(intent);
             }
         });
 
-        final Spinner keywordSpinner = getKeywordSpinner();
         final Button searchButton = (Button) findViewById(R.id.search_button);
-
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {

d770f9bf4a34214519402d3c1f13da6367c1e1f3
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2016-08-13 20:41:49 +1000

    removed an unnecessary layout resource file

--- app/src/main/java/edu/udacity/android/contentfinder/ImageDetailActivity.java
+++ app/src/main/java/edu/udacity/android/contentfinder/ImageDetailActivity.java
@@ -24,7 +24,7 @@ public class ImageDetailActivity extends AbstractMediaDetailActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_media_item_detail);
+        setContentView(R.layout.media_item_detail);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
